62dcd13408f8dedb0a7136591a3f480a92934629
CarlosAngulo30 <carlos.angulo228410@potros.itson.edu.mx>
2023-04-23 19:15:11 -0700

    Práctica 10 -Autenticación con firebase

---
+++ app/src/main/java/angulo/carlos/firebaselogin/SignInActivity.kt
@@ -0,0 +1,11 @@
+package angulo.carlos.firebaselogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
